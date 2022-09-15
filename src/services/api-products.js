c8bb39f29cb09cb9c9195785bdec8700dc016625
Alex-Taylor Danko <alexdankoxxl@gmail.com>
2022-09-15 15:14:18 +0300

    my init

--- src/services/api-products.js
+++ src/services/api-products.js
@@ -1,37 +1,33 @@
 import axios from 'axios';
-const BASE_URL = 'https://62becfba0bc9b125615fd0f7.mockapi.io/api/products'
+const BASE_URL = 'https://62becfba0bc9b125615fd0f7.mockapi.io/api/products';
 export const fetchProducts = async () => {
-    try {
-        const response = await axios.get(BASE_URL)
-        return response.data;
-    } catch (err) {
-        throw new Error(err.message)
-    }
-    
-}
-export const postProducts = async (newProduct) => {
-    try {
-        const response = await axios( {
-            body: newProduct,
-            url: BASE_URL,
-            method: 'POST',
-        })
-        return response.data;
-    } catch (err) {
-        throw new Error(err.message)
-    }
-    
-}
-export const deleteProducts = async (id) => {
-    try {
-        const response = await axios( {
-
-            url: `${BASE_URL}/${id}`,
-            method: 'DELETE',
-        })
-        return response.data;
-    } catch (err) {
-        throw new Error(err.message)
-    }
-    
-}
+  try {
+    const response = await axios.get(BASE_URL);
+    return response.data;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+export const postProducts = async newProduct => {
+  try {
+    const response = await axios({
+      body: newProduct,
+      url: BASE_URL,
+      method: 'POST',
+    });
+    return response.data;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+export const deleteProducts = async id => {
+  try {
+    const response = await axios({
+      url: `${BASE_URL}/${id}`,
+      method: 'DELETE',
+    });
+    return response.data;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
